26c9b3d3ac84a0278992d5948f0c5fdf3f147d6f
borisbolsh <bolsh.bo@yandex.ru>
2022-02-15 22:29:54 +0300

    Start onboarding module

---
+++ Foodie/App/AppDelegate.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+    window = UIWindow(frame: UIScreen.main.bounds)
+
+    if #available(iOS 13.0, *) {
+      window?.backgroundColor = .systemBackground
+    } else {
+      window?.backgroundColor = .white
+    }
+
+    window?.rootViewController = OnboardingViewController()
+    window?.makeKeyAndVisible()
+    return true
+  }
+
+}
+
---
+++ Foodie/Modules/Onboarding/OnboardingViewController.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+final class OnboardingViewController: UIViewController {
+
+  @IBOutlet weak var collectionView: UICollectionView!
+  @IBOutlet weak var nextButton: UIButton!
+  @IBOutlet weak var pageControl: UIPageControl!
+
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+
+
+  }
+
+  @IBAction func nextBtnTapped(_ sender: UIButton) {
+    
+  }
+
+}
